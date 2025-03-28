7f9c2bbc3f50a95ded8cb14ed66687530ee7bba0
Ajit <Ajit@LAPTOP-G3UQQ45K>
2025-03-28 15:04:27 +0530

    created the Demo in practice package

---
+++ Vtiger/src/test/java/practice/DemoTest.java
@@ -0,0 +1,35 @@
+package practice;
+
+import static org.testng.Assert.ARRAY_MISMATCH_TEMPLATE;
+
+public class DemoTest {
+	
+public static void main(String[] args) {
+	
+	String s="Madam";
+	
+	String rev="";
+	char a[]=     s.toCharArray();
+	
+	for(int i=a.length-1;i>=0;i--)
+	{
+		rev=rev+s.charAt(i);
+	}
+	
+	if (s.toLowerCase().equals(rev.toLowerCase())) {
+		
+		System.err.println("String is Palindrome="+rev);
+		
+	}
+	else {
+		
+		System.err.println("String is not Palindrome"+rev);
+		}
+	}
+
+	
+	
+	
+}
+
+
